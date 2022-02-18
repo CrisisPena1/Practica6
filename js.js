0f50c51c1c25e6e0c78831a7b374bf770ee9c1dc
CrisisPena1 <99937088+CrisisPena1@users.noreply.github.com>
2022-02-17 19:57:07 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,82 @@
+const cargarTipos = async()=>{
+    try{
+        const url = "http://201.140.116.237/services/tipo.php"
+        await axios 
+        .get(url)
+        .then((res)=>{
+            llenarcombos(res.data)
+        })
+        .catch((err)=>{
+            console.log("Hubo un error en la peticion" +err);
+            return false;
+        })
+    }catch{console.log("Surgio un error"); return false;}
+    return true;
+}
+function llenarcombos(data){
+    for(let item of data){
+        document.getElementById('tipo').innerHTML += `
+        <option value ="${item.tipo}">${item.descripcion}</option>
+        `
+    }
+    document.getElementById('tipo').innerHTML+=`
+    </select>
+    `
+};
+const cargarVentas=async()=>{
+    try{
+     url = "http://201.140.116.237/services/ventas.php"
+    await axios 
+    .get(url)
+    .then((res)=>{
+        dibujarTabla(res.data);
+    })
+    .catch((err)=>{
+        console.log("Hubo un error en la peticion" +err);
+        
+    })
+}catch{console.log("Surgio un error")}
+}
+function dibujarTabla(data){
+let tipo = document.getElementById("tipo").value;
+document.getElementById("ventas").innerHTML=`
+        <tr id="tab">
+            <th>Folio</th>
+            <th>Tipo</th>
+            <th>Precio</th>
+            <th>Descuento</th>
+            <th>Total</th>
+            <th>Fecha Pago</th>
+            <th >Giro</th>
+        </tr>`
+for(let item of data){
+    if(item.tipo == tipo){
+        document.getElementById("ventas").innerHTML+=`
+      
+        <tr>
+        <td class="rt">${item.folio}</td>
+        <td class="rt">${item.tipo}</td>
+        <td class="rt">${item.precio}</td>
+        <td class="rt">${item.descuento}</td>
+        <td class="rt">${item.total}</td>
+        <td class="rt">${item.fechapago}</td>
+        <td class="rt">${item.giro}</td>
+    </tr>
+        `
+    }
+}
+}
+
+
+
+const inicia=async()=>{
+    if(await cargarTipos() == true){
+        cargarVentas();
+    }
+    document.getElementById("tipo").addEventListener("change",async()=>{
+    cargarVentas();
+});
+    
+}
+
+inicia();
